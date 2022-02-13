21f0e3479ce43a0c040ab202c55daab5c0e0a1dc
Rasujon3 <rasujon3@gmail.com>
2022-02-13 20:39:54 +0600

    grand total calculation done

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ addProductBtn.addEventListener("click", function () {
   const td2 = document.createElement("td");
   const td3 = document.createElement("td");
 
+  td3.classList.add("item-total");
   th.innerText = itemName.value; // <th >1</th>
   td1.innerText = itemPrice.value; // <td>100</td>
   td2.innerText = itemQuantity.value;
@@ -29,7 +30,7 @@ addProductBtn.addEventListener("click", function () {
   tr.appendChild(th);
   tr.appendChild(td1);
   tr.appendChild(td2);
-  tr.appendChild(td3);
+  tr.appendChild(td3); // <td class="item-total">4</td>
 
   infoTale.appendChild(tr);
 
@@ -37,4 +38,31 @@ addProductBtn.addEventListener("click", function () {
   itemName.value = "";
   itemPrice.value = "";
   itemQuantity.value = "";
+  totalCalculation();
 });
+
+function totalCalculation() {
+  const subTotal = calculateSubTotal();
+  const subTotalToDisplay = document.getElementById("sub-total");
+  subTotalToDisplay.innerText = subTotal;
+  // tax calculation
+  const tax = subTotal * 0.2;
+  document.getElementById("tax").innerText = tax.toFixed(2);
+  // grand total calculation
+  const grandTotal = subTotal + tax;
+  document.getElementById("grand-total").innerText = grandTotal;
+  document.getElementById("grand-total-2").innerText = grandTotal;
+}
+
+function calculateSubTotal() {
+  let subTotal = 0;
+  const cost = document.getElementsByClassName("item-total");
+  // console.log("all prices", cost);
+  for (let i = 0; i < cost.length; i++) {
+    const element = cost[i];
+    // console.log(element.innerText);
+    const price = parseFloat(element.innerText);
+    subTotal = subTotal + price;
+  }
+  return subTotal;
+}
